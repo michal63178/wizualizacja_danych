d404718a7a602c6d4bcc46e67b0e606eaadfae0b
michal63178 <michal.jakubowski.730@gmail.com>
2021-03-09 22:03:58 +0100

    Gotowe

--- zadania 1.py
+++ zadania 1.py
@@ -9,37 +9,95 @@ i, j = [e, f], [a, c]
 k, L = {1: 'a'}, {2: 'c'}
 m, n = {4, 5, 6}, {9, 8, 7}
 
-print(a, b, c, d, e, f, g,
-      h, i, j, k, L, m, n)
+print(a, b, c, d, e, f, g, h, i, j, k, L, m, n)
+
 
 # 2
-a = float(input())
-b = float(input())
-c = input()
 
-if c == '+':
-    print(a + b)
 
-elif c == '-':
-    print(a - b)
+def correct(number):
+    if not number:
+        return False
 
-elif c == '*':
-    print(a * b)
+    if number.count('-') > 1:
+        return False
 
-elif c == '/':
-    print(a / b)
+    if number.count('.') > 1:
+        return False
 
-elif c == '//':
-    print(a // b)
+    idx = number.find('-') - 1
 
-elif c == '^':
-    print(a ** b)
+    if idx != -1 and idx != -2:
+        return False
 
-elif c == 'prw':
-    print(math.sqrt(a))
+    number = set(number)
 
-elif c == 'mod':
-    print(a % b)
+    for char in number:
+        if char not in '.-0123456789':
+            return False
+
+    return True
+
+
+a = input('Podaj liczbę\n')
+sign = input('Wpisz odpowiedni znak, aby wybrać działanie\n'
+             'dodawanie: +\n'
+             'odejmowanie: -\n'
+             'mnożenie: *\n'
+             'dzielenie: /\n'
+             'potęgowanie: ^\n'
+             'pierwiastkowanie: prw\n'
+             'dzielenie całkowite: //\n'
+             'reszta z dzielenia: mod\n')
+
+if sign == 'prw':
+    if correct(a):
+        a = float(a)
+
+        if a >= 0:
+            print(math.sqrt(a))
+
+        else:
+            print('Brak rozwiązań w zbiorze liczb rzeczywistych')
+
+    else:
+        print('Złe dane')
+
+else:
+    b = input('Podaj liczbę\n')
+
+    if (correct(a) and correct(b) and sign in
+            ('+', '-', '*', '/', '^', '//', 'mod')):
+        a = float(a)
+        b = float(b)
+
+        if sign == '+':
+            print(a + b)
+
+        elif sign == '-':
+            print(a - b)
+
+        elif sign == '*':
+            print(a * b)
+
+        elif sign == '^':
+            print(a ** b)
+
+        elif b:
+            if sign == '/':
+                print(a / b)
+
+            elif sign == '//':
+                print(a // b)
+
+            elif sign == 'mod':
+                print(a % b)
+
+        else:
+            print('Nieokreślone')
+
+    else:
+        print('Złe dane')
 
 # 3
 a = 4
@@ -63,12 +121,12 @@ last_name = 'NOWAK'
 print(name.capitalize(), last_name.capitalize())
 
 # 6
-lyrics = 'Jest wesoło la la la wszyscy się bawimy la la la'
+lyrics = 'Jest wesoło la la la bawimy się la la la'
 
 print(lyrics.count('la'))
 
 # 7
-text = 'To jest przykładowy ciąg znaków.'
+text = 'To jest przykładowy ciąg znaków'
 
 print(text[1], text[-1])
 
